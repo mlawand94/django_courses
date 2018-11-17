34e5757dfe99e2db4874b31a9c842b5f70307349
mlawand94 <mlawand94@gmail.com>
2018-11-17 15:08:59 -0600

    Models Complete

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'^$', views.hello_world),
+	url(r'^courses/', include('courses.urls')),
+    url(r'admin/', admin.site.urls),
+    url(r'^$', views.hello_world),
 ]
