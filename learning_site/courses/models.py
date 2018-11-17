e57a5c96860cea53833934bc93e2ad710477c674
mlawand94 <mlawand94@gmail.com>
2018-11-17 11:36:54 -0600

    Courses migration implemented

--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -3,6 +3,5 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add = True)
-    title = models.Char(max_length=255)
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    
