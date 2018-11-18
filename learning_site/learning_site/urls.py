3b36f7ed1a70fd90cd39e16dae256bf73f423445
mlawand94 <mlawand94@gmail.com>
2018-11-17 19:01:28 -0600

    templates Implemented, before implementing class, 'Steps'

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from . import views
+
 
 urlpatterns = [
 	url(r'^courses/', include('courses.urls')),
     url(r'admin/', admin.site.urls),
     url(r'^$', views.home),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
