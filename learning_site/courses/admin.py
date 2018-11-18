7d875f58debf7d1be8d48000f9c46dd81b8a81e2
mlawand94 <mlawand94@gmail.com>
2018-11-18 00:00:17 -0600

    Django templates complete and 404 implemented

--- learning_site/courses/admin.py
+++ learning_site/courses/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Step
+
+class StepInline(admin.StackedInline):
+	model = Step
+
+class courseAdmin(admin.ModelAdmin):
+	inlines = [StepInline, ]
+
 # Register your models here.
-admin.site.register(Course)
+admin.site.register(Course, courseAdmin)
+admin.site.register(Step)
